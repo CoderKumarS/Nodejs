bbafeaa78aa57ccffe571322928c42d9725b53f5
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2024-01-18 10:43:39 +0530

    While Loop and Do-while loop

---
+++ Looping/doWhileLoop.js
@@ -0,0 +1,8 @@
+// first it execute then check condition
+// also known as exit control
+
+let count = 0;
+do {
+    console.log(count);
+    count++;
+} while (count < 5);
--- Looping/whileLoop.js
+++ Looping/whileLoop.js
@@ -1,5 +1,8 @@
+// first it check condition then execute 
+// also known as entry control
+
 let count = 0;
-while (count<5) {
+while (count < 5) {
     console.log(count);
     count++;
 }
