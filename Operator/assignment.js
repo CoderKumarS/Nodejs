d60b209dd54c17c73907a375b423d6b2757e6f16
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2024-01-18 10:33:49 +0530

    Assignment  Operator

---
+++ Operator/assignment.js
@@ -0,0 +1,7 @@
+let num = 10;
+
+num += 5; //Equivalent to: num: num + 5
+num -= 2;//Equivalent to: num: num - 5
+num *= 3;//Equivalent to: num: num * 5
+num /= 4;//Equivalent to: num: num / 5
+console.log(num);   
