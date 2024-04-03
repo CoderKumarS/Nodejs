0dd94d5769acacfa8d74fb0ae53d8e4ccf562da1
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2024-04-03 21:10:10 +0530

    Update middleAuth.js

--- ExpressJs/middleAuth.js
+++ ExpressJs/middleAuth.js
@@ -4,36 +4,46 @@ const fs = require('fs');
 const app = express()
 const port = 3000
 app.use(express.urlencoded({ extended: true }));
+
 const users = [
     { id: 1, username: 'John', password: 'Wick' },
     { id: 2, username: 'Babu', password: 'Rao' }
 ];
+
 app.use('/profile', (req, res, next) => {
     const { username, password } = req.body;
-    const user = users.find(u => username === username && password === password);
+    const user = users.find(u => u.username === username && u.password === password);
     if (user) {
         req.user = user;
         next();
     } else {
-        res.status(401).send('unauthorized');
+        res.status(401).send('Unauthorized');
     }
 });
+
 app.get('/', (req, res) => {
-    res.send('Welcome to public route!');
+    res.send('Welcome to the public route!');
 });
+
 app.get('/login', (req, res) => {
     fs.readFile(__dirname + '/form2.html', 'utf-8', (err, data) => {
         if (err) {
             console.error(err);
-            res.send("Error Displaying Form");
+            return res.send("Error Displaying Form");
         } else {
             res.send(data);
         }
     });
 });
-app.post('/profile', (req,res)=>{
-    res.send(`Welcome ${req.user.username}!`);
+
+app.post('/profile', (req, res) => {
+    if (req.user) {
+        res.send(`Welcome ${req.user.username}!`);
+    } else {
+        res.status(401).send('Unauthorized');
+    }
 });
-app.listen(port, () =>
-    console.log(`Example app listening on port ${port}!`)
-);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
